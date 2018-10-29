a1815506e8e4448fb487510613e0d6c3fac43a4e
Bradley <p_bradley@tss-hr420-dm07.fc.ca>
2018-10-29 14:50:31 -0400

    final

--- game.py
+++ game.py
@@ -5,10 +5,10 @@ from random import randint
 player_lives = 3
 computer_lives = 3
 
-
 # choices is an array => a container that can hold multiple items
 
 # arrays are 0-based -> the first item is 0, the second is 1, etc
+print("Welcome to Rock, Paper, Scissors!")
 
 choices = ["Rock", "Paper", "Scissors"]
 
@@ -27,55 +27,41 @@ print("Computer chooses: ", computer, "\n")
 
 while player is False:
     # set player to True
-    print("Players Lives:", player_lives, "/5")
-    print("Computer Lives:", computer_lives, "/5")
+    print("Players Lives:", player_lives, "/3")
+    print("Computer Lives:", computer_lives, "/3")
     print("Choose your weapon!")
     player = input("Rock, Paper, or Scissors?\n")
     print(player, "\n")
 
-
-if player == computer:
-
-            print("TIE! You live to shoot another day")
-
-elif player == "Rock":
-
+    if player == computer:
+        print("Tie!\n")
+    elif player == "Rock":
         if computer == "Paper":
-            player_lives = - 1
-            print("YOU LOSE! Paper covers Rock!")
-
+            player_lives -= 1
+            print("You lose!", computer, "covers", player, "\n")
         else:
-            computer_lives = - 1
-            print("YOU WIN!", player, "smashes", computer)
-
-elif player == "Paper":
-
-        if computer == "scissors":
-            player_lives = - 1
-            print("YOU LOSE!", computer, "cuts", player)
-
+            computer_lives -= 1
+            print("You win!", player, "smashes", computer, "\n")
+    elif player == "Paper":
+        if computer == "Scissors":
+            player_lives -= 1
+            print("You lose!", computer, "cut", player, "\n")
         else:
-            computer_lives = - 1
-            print("YOU WIN!", player, "covers", computer)
-
-
-elif player == "Scissors":
-
+            computer_lives -= 1
+            print("You win!", player, "covers", computer, "\n")
+    elif player == "Scissors":
         if computer == "Rock":
-            player_lives = - 1
-            print("YOU LOSE!", computer, "smashes", player)
-
+            player_lives -= 1
+            print("You lose!", computer, "smashes", player, "\n")
         else:
-            computer_lives = - 1
-            print("YOU WIN!", player, "cuts", computer)
-
-elif player == "quit":
-
+            computer_lives -= 1
+            print("You win!", player, "cut", computer, "\n")
+    elif player == "quit":
         exit()
+    
+    else:
 
-else:
-
-    print("Check your spelling... That's not a valid choice")
+        print("Check your spelling... That's not a valid choice")
 
     if player_lives is 0:
         print("You are out of lives. Would you like to play again?")
@@ -104,7 +90,6 @@ else:
         elif (choice is "N") or (choice is "n"):
             print("Thanks for playing!")
             exit()
-
-player = False
-
-computer = choices[randint(0, 2)]
+    else:
+        player = False
+        computer = choices[randint(0, 2)]
